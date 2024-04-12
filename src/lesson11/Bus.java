e9f4eed7afd76be3df8196850c05290ca290c1e3
caramavrovmihail <caramavrov2001@gmail.com>
2024-04-12 20:52:42 +0300

    add bus commit

--- src/lesson11/Bus.java
+++ src/lesson11/Bus.java
@@ -2,45 +2,35 @@ package lesson11;
 
 public class Bus extends Transport {
     @Override
-    public void printInfo(){
-<<<<<<< HEAD
-
-=======
+    public void printInfo() {
         super.printInfo();
->>>>>>> origin/master
         System.out.println("This is a bus");
     }
+
     @Override
-    public void go(){
-
+    public void go() {
         System.out.println("This bus can go");
-
     }
-    public void calculateDistance(){
+
+    public void calculateDistance() {
         int speed = 60;
         int time = 60;  // second
-        int distance = time*speed;
-        System.out.println("Distance that bus can do in 60 seconds: "+distance);
-
+        int distance = time * speed;
+        System.out.println("Distance that bus can do in 60 seconds: " + distance);
     }
-    public void calculateDistance(int time){
-        int distance = time* 90;
-        System.out.println("The max distance, if speed is 90km/h is: "+distance );
 
-
+    public void calculateDistance(int time) {
+        int distance = time * 90;
+        System.out.println("The max distance, if speed is 90km/h is: " + distance);
     }
-    public static void main(String[] arg){
+
+    public static void main(String[] arg) {
         Transport myTransport = new Car();
         myTransport.printInfo();
-<<<<<<< HEAD
-      myTransport.calculateDistance(90);
-      myTransport.calculateDistance();
+        myTransport.calculateDistance(90);
+        myTransport.calculateDistance();
         myTransport.go();
-=======
         ((Car) myTransport).calculateDistance(90);
         ((Car) myTransport).calculateDistance();
->>>>>>> origin/master
-
-
     }
 }
