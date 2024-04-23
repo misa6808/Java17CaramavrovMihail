254a4fe5f5af5a9ed619c91d3e4b183bf19dbbde
caramavrovmihail <caramavrov2001@gmail.com>
2024-04-23 19:27:37 +0300

    add hw14

---
+++ src/lesson17/NotBeltException.java
@@ -0,0 +1,8 @@
+package lesson17;
+// Второе пусть является дочерним классом класса Exception и называется NotBeltException.
+// Также пусть передает параметр String в конструктор super класса.
+class NotBeltException extends Exception {
+    public NotBeltException(String message) {
+        super(message);
+    }
+}
